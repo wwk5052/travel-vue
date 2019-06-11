ca8544ec2f14427ec05499dfba90e6d5efc694b2
wang <wangwenkai5052@163.com>
2019-06-11 09:54:53 +0800

    project init

--- travel-vue/src/main.js
+++ travel-vue/src/main.js
@@ -1,15 +1,17 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import Vue from "vue";
+import App from "./App";
+import router from "./router";
+//解决移动端高像素(二倍屏)问题，引入border.css即可
+import "./assets/style/border.css";
+import "./assets/style/reset.css";
+//移动端点击300ms延时问题
+import fastClick from "fastclick";
 
-Vue.config.productionTip = false
-
-/* eslint-disable no-new */
+Vue.config.productionTip = false;
+fastClick.attach(document.body);
 new Vue({
-  el: '#app',
+  el: "#app",
   router,
   components: { App },
-  template: '<App/>'
-})
+  template: "<App/>"
+});
--- travel-vue/src/pages/home/Home.vue
+++ travel-vue/src/pages/home/Home.vue
@@ -1,14 +1,14 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+  <div>这是Home页面</div>
 </template>
+
 
 <script>
 export default {
-  name: "App"
+  name: "Home"
 };
 </script>
 
-<style>
+
+<style scope>
 </style>
