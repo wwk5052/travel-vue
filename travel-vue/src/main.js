73d249d8552e3f546a7ab8e19aaec2dddebf2bd1
wang <wangwenkai5052@163.com>
2019-06-11 15:22:09 +0800

    swiper

--- travel-vue/src/main.js
+++ travel-vue/src/main.js
@@ -4,11 +4,16 @@ import router from "./router";
 //解决移动端高像素(二倍屏)问题，引入border.css即可
 import "./assets/style/border.css";
 import "./assets/style/reset.css";
+import VueAwesomeSwiper from "vue-awesome-swiper";
+import "swiper/dist/css/swiper.css";
 //移动端点击300ms延时问题
 import fastClick from "fastclick";
 
 Vue.config.productionTip = false;
 fastClick.attach(document.body);
+
+Vue.use(VueAwesomeSwiper /* { default global options } */);
+
 new Vue({
   el: "#app",
   router,
---
+++ travel-vue/src/pages/home/components/Swiper.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="swiper">
+    <swiper ref="mySwiper" :options="swiperOption">
+      <!-- slides -->
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img :src="item.imgUrl" alt>
+      </swiper-slide>
+      <!-- Optional controls -->
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+
+
+<script>
+import "swiper/dist/css/swiper.css";
+import { swiper, swiperSlide } from "vue-awesome-swiper";
+
+export default {
+  name: "HomeSwiper",
+  data() {
+    return {
+      swiperOption: {
+        pagination: ".swiper-pagination",
+        loop: true
+      },
+      swiperList: [
+        {
+          id: "001",
+          imgUrl:
+            "http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20195/fdff7e5abe30ec5f014b4d7464e0c3ef.jpg_750x200_d1d693c8.jpg"
+        },
+        {
+          id: "002",
+          imgUrl:
+            "http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20193/d7bbc21db442366a882e04ddc984669a.jpg_750x200_85e640d9.jpg"
+        },
+        {
+          id: "003",
+          imgUrl:
+            "http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20193/87a224d0349d94a11e97f31aa1aba4f5.jpg_750x200_1f78af87.jpg"
+        },
+        {
+          id: "004",
+          imgUrl:
+            "http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20195/aef5f8864f5a1167599609307571b2c4.jpg_750x200_db7bedbe.jpg"
+        }
+      ]
+    };
+  },
+  components: {
+    swiper,
+    swiperSlide
+  }
+};
+</script>
+
+
+<style lang="stylus" scoped>
+.swiper >>>swiper-pagination-bullet-active {
+  background-color: #fff;
+}
+
+.swiper {
+  width: 100%;
+  height: 2rem;
+  background: #eee;
+  color: #00afc7;
+
+  img {
+    display: block;
+    width: 100%;
+    height: 2rem;
+  }
+}
+</style>
