ccf8f9d6a37809ceb8afbdca907efc6c6d971dd4
wang <wangwenkai5052@163.com>
2019-06-12 16:31:56 +0800

    city-router

---
+++ travel-vue/src/pages/city/components/Header.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="header">
+    城市选择
+    <router-link to="/">
+      <div class="iconfont header-back">&#xe682;</div>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CityHeader"
+};
+</script>
+
+<style lang="stylus" scoped>
+.header {
+  overflower: hidden;
+  height: 0.86rem;
+  line-height: 0.86rem;
+  text-align: center;
+  color: #fff;
+  background-color: #00bcd4;
+  font-size: 0.32rem;
+  position: relative;
+
+  .header-back {
+    position: absolute;
+    top: 0;
+    left: 0;
+    text-align: center;
+    font-size: 0.4rem;
+    width: 0.64rem;
+    color: #fff;
+  }
+}
+</style>
--- travel-vue/src/pages/home/components/Header.vue
+++ travel-vue/src/pages/home/components/Header.vue
@@ -7,10 +7,12 @@
       <i class="iconfont magnifier">&#xe623;</i>
       输入城市/景点/游玩/主题
     </div>
-    <div class="header-right">
-      {{this.city}}
-      <i class="iconfont">&#xe627;</i>
-    </div>
+    <router-link to="/city">
+      <div class="header-right">
+        {{this.city}}
+        <i class="iconfont">&#xe627;</i>
+      </div>
+    </router-link>
   </div>
 </template>
 
@@ -64,6 +66,7 @@ export default {
     width: 1.24rem;
     float: right;
     text-align: center;
+    color: #fff;
   }
 }
 </style>
