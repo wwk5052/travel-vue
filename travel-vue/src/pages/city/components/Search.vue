a2b61babf1055f0f7cda4eb98be7422bfd6d673a
wang <wangwenkai5052@163.com>
2019-06-13 15:47:48 +0800

    city-input

--- travel-vue/src/pages/city/components/Search.vue
+++ travel-vue/src/pages/city/components/Search.vue
@@ -1,13 +1,64 @@
 <template>
-  <div class="search">
-    <input type="input" class="search-input" placeholder="输入城市名或拼音">
+  <div>
+    <div class="search">
+      <input v-model="keyword" type="input" class="search-input" placeholder="输入城市名或拼音">
+    </div>
+    <div class="search-content" ref="search" v-show="keyword">
+      <ul>
+        <li :key="item.id" v-for="item of list" class="search-item border-bottom">{{item.name}}</li>
+        <li class="search-item border-bottom" v-show="hasNoData">没有找到匹配数据</li>
+      </ul>
+    </div>
   </div>
 </template>
 
 
 <script>
+import Bscroll from "better-scroll";
+
 export default {
-  name: "CitySearch"
+  name: "CitySearch",
+  props: { cities: Object },
+  data() {
+    return {
+      keyword: "",
+      list: [],
+      timer: null
+    };
+  },
+  watch: {
+    keyword() {
+      if (this.timer) {
+        clearTimeout(this.timer);
+      }
+      if (!this.keyword) {
+        this.list = [];
+        return;
+      }
+      this.timer = setTimeout(() => {
+        const result = [];
+        for (let i in this.cities) {
+          this.cities[i].forEach(value => {
+            if (
+              value.spell.indexOf(this.keyword) > -1 ||
+              value.name.indexOf(this.keyword) > -1
+            ) {
+              result.push(value);
+            }
+          });
+        }
+        this.list = result;
+      }, 100);
+    }
+  },
+  mounted() {
+    this.scroll = new Bscroll(this.$refs.search);
+  },
+  computed: {
+    hasNoData() {
+      return !this.list.length;
+    }
+  }
 };
 </script>
 
@@ -27,4 +78,22 @@ export default {
     border-radius: 0.06rem;
     color: #666;
   }
-}</style>
+}
+
+.search-content {
+  position: absolute;
+  top: 1.58rem;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  overflow: hidden;
+  z-index: 1;
+  background-color: #fff;
+
+  .search-item {
+    line-height: 0.62rem;
+    color: #666;
+    padding-left: 0.2rem;
+  }
+}
+</style>
